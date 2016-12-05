ee301d8f42fb3beae01a7817767a9b28aacbaadd
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-12-05 10:44:14 +0900

    テストを修正。

--- dromozoa-vecmath-test.js
+++ dromozoa-vecmath-test.js
@@ -19,108 +19,115 @@
 (function (root) {
   "use strict";
   var Math = root.Math;
-  var console = root.console;
   var $ = root.jQuery;
   var vecmath = root.dromozoa.vecmath;
+  var Tuple2 = vecmath.Tuple2;
+  var Vector2 = vecmath.Vector2;
+  var Matrix3 = vecmath.Matrix3;
   var epsilon = 2.22044604925031e-16;
 
-  function assert(result, message) {
+  function test(result) {
+    if (root.console && root.console.assert) {
+      root.console.assert(result);
+    }
+    test.count += 1;
     if (result) {
-      assert.count += 1;
-      return result;
+      test.passed += 1;
+      root.console.log("test " + test.count + " is passed");
+    } else {
+      test.failed += 1;
+      root.console.log("test " + test.count + " is failed");
     }
-    if (!message) {
-      message = "assertion failed!";
-    }
-    var e = new root.Error(message);
-    console.log(e, e.stack);
-    throw e;
   }
 
-  assert.count = 0;
-
-  var Tuple2 = assert(vecmath.Tuple2);
-  var Vector2 = assert(vecmath.Vector2);
-  var Matrix3 = assert(vecmath.Matrix3);
+  test.count = 0;
+  test.passed = 0;
+  test.failed = 0;
 
   $(function () {
-    assert(new Tuple2(-1, -2).absolute().equals({ x: 1, y: 2 }));
-    assert(new Vector2(0, 1).scale(2).length() === 2);
-    assert(new Tuple2(1, 2).add({ x: 3, y: 4 }).equals({ x: 4, y: 6 }));
-    assert(new Tuple2(1, 2).sub({ x: 3, y: 4 }).equals({ x: -2, y: -2 }));
-    assert(new Tuple2(1, 2).interpolate(new Tuple2(5, 10), 0.25).equals(new Tuple2(2, 4)));
+    $("body")
+      .append($("<div>")
+        .addClass("container dromozoa-vecmath")
+        .css("margin-top", "10px"));
+
+    test(new Tuple2(-1, -2).absolute().equals({ x: 1, y: 2 }));
+    test(new Vector2(0, 1).scale(2).length() === 2);
+    test(new Tuple2(1, 2).add({ x: 3, y: 4 }).equals({ x: 4, y: 6 }));
+    test(new Tuple2(1, 2).sub({ x: 3, y: 4 }).equals({ x: -2, y: -2 }));
+    test(new Tuple2(1, 2).interpolate(new Tuple2(5, 10), 0.25).equals(new Tuple2(2, 4)));
 
     var t1 = new Tuple2(1, 2);
     var t2 = t1.clone().scale(2);
-    assert(!t1.equals(t2));
-    assert(t2.equals({ x: 2, y: 4 }));
+    test(!t1.equals(t2));
+    test(t2.equals({ x: 2, y: 4 }));
 
-    assert(new Tuple2(1, -2).negate().equals(new Tuple2(-1, 2)));
-    assert(new Tuple2(1, 42).clamp(17, 23).equals(new Tuple2(17, 23)));
-    assert(new Tuple2(1, 2).scale_add(2, { x: 4, y: 1 }).equals(new Tuple2(6, 5)));
-    assert(new Tuple2().equals(new Tuple2(0, 0)));
+    test(new Tuple2(1, -2).negate().equals(new Tuple2(-1, 2)));
+    test(new Tuple2(1, 42).clamp(17, 23).equals(new Tuple2(17, 23)));
+    test(new Tuple2(1, 2).scale_add(2, { x: 4, y: 1 }).equals(new Tuple2(6, 5)));
+    test(new Tuple2().equals(new Tuple2(0, 0)));
 
-    assert(new Vector2(3, 4).length_squared() === 25);
-    assert(new Vector2(3, 4).length() === 5);
-    assert(new Vector2(1, 2).dot(new Vector2(3, 4)) === 11);
-    assert(new Vector2(1, 1).angle(new Vector2(-1, 1)) === Math.PI / 2);
-    assert(new Vector2(2, 0).normalize().equals(new Vector2(1, 0)));
-    assert(new Vector2(0, 3).normalize().equals(new Vector2(0, 1)));
-    assert(new Vector2(3, 4).clone().length() === 5);
+    test(new Vector2(3, 4).length_squared() === 25);
+    test(new Vector2(3, 4).length() === 5);
+    test(new Vector2(1, 2).dot(new Vector2(3, 4)) === 11);
+    test(new Vector2(1, 1).angle(new Vector2(-1, 1)) === Math.PI / 2);
+    test(new Vector2(2, 0).normalize().equals(new Vector2(1, 0)));
+    test(new Vector2(0, 3).normalize().equals(new Vector2(0, 1)));
+    test(new Vector2(3, 4).clone().length() === 5);
 
     var m = new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9);
-    assert(m.m00 === 1 && m.m01 === 2 && m.m02 === 3);
-    assert(m.m10 === 4 && m.m11 === 5 && m.m12 === 6);
-    assert(m.m20 === 7 && m.m21 === 8 && m.m22 === 9);
+    test(m.m00 === 1 && m.m01 === 2 && m.m02 === 3);
+    test(m.m10 === 4 && m.m11 === 5 && m.m12 === 6);
+    test(m.m20 === 7 && m.m21 === 8 && m.m22 === 9);
 
     m = m.clone();
-    assert(m.m00 === 1 && m.m01 === 2 && m.m02 === 3);
-    assert(m.m10 === 4 && m.m11 === 5 && m.m12 === 6);
-    assert(m.m20 === 7 && m.m21 === 8 && m.m22 === 9);
+    test(m.m00 === 1 && m.m01 === 2 && m.m02 === 3);
+    test(m.m10 === 4 && m.m11 === 5 && m.m12 === 6);
+    test(m.m20 === 7 && m.m21 === 8 && m.m22 === 9);
 
-    assert(m.equals(m.clone()));
-    assert(new Matrix3().set_zero().equals(new Matrix3(0, 0, 0, 0, 0, 0, 0, 0, 0)));
-    assert(new Matrix3().set_identity().equals(new Matrix3(1, 0, 0, 0, 1, 0, 0, 0, 1)));
-    assert(m.clone().set_row(0, 11, 12, 13).equals(new Matrix3(11, 12, 13, 4, 5, 6, 7, 8, 9)));
-    assert(m.clone().set_row(1, 14, 15, 16).equals(new Matrix3(1, 2, 3, 14, 15, 16, 7, 8, 9)));
-    assert(m.clone().set_row(2, 17, 18, 19).equals(new Matrix3(1, 2, 3, 4, 5, 6, 17, 18, 19)));
-    assert(m.clone().set_column(0, 11, 14, 17).equals(new Matrix3(11, 2, 3, 14, 5, 6, 17, 8, 9)));
-    assert(m.clone().set_column(1, 12, 15, 18).equals(new Matrix3(1, 12, 3, 4, 15, 6, 7, 18, 9)));
-    assert(m.clone().set_column(2, 13, 16, 19).equals(new Matrix3(1, 2, 13, 4, 5, 16, 7, 8, 19)));
-    assert(m.clone().transpose().equals(new Matrix3(1, 4, 7, 2, 5, 8, 3, 6, 9)));
+    test(m.equals(m.clone()));
+    test(new Matrix3().set_zero().equals(new Matrix3(0, 0, 0, 0, 0, 0, 0, 0, 0)));
+    test(new Matrix3().set_identity().equals(new Matrix3(1, 0, 0, 0, 1, 0, 0, 0, 1)));
+    test(m.clone().set_row(0, 11, 12, 13).equals(new Matrix3(11, 12, 13, 4, 5, 6, 7, 8, 9)));
+    test(m.clone().set_row(1, 14, 15, 16).equals(new Matrix3(1, 2, 3, 14, 15, 16, 7, 8, 9)));
+    test(m.clone().set_row(2, 17, 18, 19).equals(new Matrix3(1, 2, 3, 4, 5, 6, 17, 18, 19)));
+    test(m.clone().set_column(0, 11, 14, 17).equals(new Matrix3(11, 2, 3, 14, 5, 6, 17, 8, 9)));
+    test(m.clone().set_column(1, 12, 15, 18).equals(new Matrix3(1, 12, 3, 4, 15, 6, 7, 18, 9)));
+    test(m.clone().set_column(2, 13, 16, 19).equals(new Matrix3(1, 2, 13, 4, 5, 16, 7, 8, 19)));
+    test(m.clone().transpose().equals(new Matrix3(1, 4, 7, 2, 5, 8, 3, 6, 9)));
 
     m = new Matrix3(1, 2, 1, 2, 1, 0, 1, 1, 2);
-    assert(m.determinant() === -5);
+    test(m.determinant() === -5);
     m.invert();
-    assert(m.m00 === -0.4 && m.m01 ===  0.6 && m.m02 ===  0.2);
-    assert(m.m10 ===  0.8 && m.m11 === -0.2 && m.m12 === -0.4);
-    assert(m.m20 === -0.2 && m.m21 === -0.2 && m.m22 ===  0.6);
+    test(m.m00 === -0.4 && m.m01 ===  0.6 && m.m02 ===  0.2);
+    test(m.m10 ===  0.8 && m.m11 === -0.2 && m.m12 === -0.4);
+    test(m.m20 === -0.2 && m.m21 === -0.2 && m.m22 ===  0.6);
 
     var v = new Matrix3().set_identity().rot_z(Math.PI / 4).transform(new Vector2(1, 1).normalize());
-    assert(v.epsilon_equals(new Vector2(0, 1), epsilon));
+    test(v.epsilon_equals(new Vector2(0, 1), epsilon));
 
-    assert(new Matrix3().equals(new Matrix3(0, 0, 0, 0, 0, 0, 0, 0, 0)));
+    test(new Matrix3().equals(new Matrix3(0, 0, 0, 0, 0, 0, 0, 0, 0)));
 
-    assert(new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    test(new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9)
       .add(new Matrix3(9, 8, 7, 6, 5, 4, 3, 2, 1))
       .equals(new Matrix3(10, 10, 10, 10, 10, 10, 10, 10, 10)));
-    assert(new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    test(new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9)
       .sub(new Matrix3(9, 8, 7, 6, 5, 4, 3, 2, 1))
       .equals(new Matrix3(-8, -6, -4, -2, 0, 2, 4, 6, 8)));
-    assert(new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    test(new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9)
       .mul(new Matrix3(9, 8, 7, 6, 5, 4, 3, 2, 1))
       .equals(new Matrix3(30, 24, 18, 84, 69, 54, 138, 114, 90)));
 
     m = new Matrix3().rot_z(Math.PI / 4);
     m = m.mul(m);
-    assert(m.epsilon_equals(new Matrix3().rot_z(Math.PI / 2), epsilon));
+    test(m.epsilon_equals(new Matrix3().rot_z(Math.PI / 2), epsilon));
 
-    assert(new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    test(new Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9)
       .negate()
       .equals(new Matrix3(-1, -2, -3, -4, -5, -6, -7, -8, -9)));
 
-    $("body")
-      .append($("<div>")
-        .text(assert.count + " assertions are passed"));
+    root.console.log(test.passed + " tests are passed");
+    if (test.failed > 0) {
+      root.console.log(test.failed + " tests are failed");
+    }
   });
 }(this.self));
